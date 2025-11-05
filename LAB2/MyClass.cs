1bc2d0bd5594d4fcc6602c853433caf268c068a8
KAGS <sergej.naumov.0606@mail.ru>
2025-11-05 18:54:39 +0700

    а

---
+++ LAB2/MyClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    public class Trial
+    {
+        public string Name { get; set; }    
+        public string Time { get; set; }
+        public Trial() { }
+    }
+
+    public class Exam : Trial 
+    {
+        public int QuestionCount { get; set; }
+        public int Subject { get; set; }
+    }
+
+    public class FinalExam : Exam
+    {
+        
+    }
+
+    public class Test : Trial
+    {
+        public int QuestionCount { get; set; }
+    }
+
+    public class Question : Test
+    {
+
+    }
+}
